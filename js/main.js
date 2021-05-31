655e8d355940639535f08fd818990434675bb50d
Шамиль Магомедов <s.magomedov@m-games-ltd.com>
2021-05-31 18:49:59 +0300

    #module-task1 #fix #script.js - fix swapRangeValue remove and add inline if

--- js/main.js
+++ js/main.js
@@ -2,15 +2,12 @@ function getRandomIntegerNumber(min, max) {
   if (min <= 0) {
     min = 0;
   }
-  console.log('Значения до свапа: ' + min, max);
   if (max < min) {
     const temp = min;
     min = max;
     max = temp;
   }
-  console.log('Значения после свапа: ' + min, max);
   const rand = Math.round(Math.random() * (max - min) + min);
-  console.log('Случайное число: ' + rand);
   return rand;
 }
 
@@ -18,15 +15,12 @@ function getRandomFractionalNumber(min, max, quantityCharAfterComma) {
   if (min <= 0) {
     min = 0;
   }
-  console.log('Значения до свапа: ' + min, max);
   if (max < min) {
     const temp = min;
     min = max;
     max = temp;
   }
-  console.log('Значения после свапа: ' + min, max);
   const rand = (Math.random() * (max - min) + min).toFixed(quantityCharAfterComma);
-  console.log('Случайное число: ' + rand);
   return rand;
 }
 
